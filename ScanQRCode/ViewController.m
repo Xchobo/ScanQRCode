2ecd9043ab0dc09ad86c29b214482a8a04e6dfdb
Xchobo <xchobo@gmail.com>
2014-03-19 12:00:04 +0800

    ScanQRCode

--- ScanQRCode/ViewController.m
+++ ScanQRCode/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "QRCodeViewController.h"
+
+#define IOS7 [[[UIDevice currentDevice] systemVersion]floatValue]>=7
 
 @interface ViewController ()
+<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UILabel *messageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *qrcodedataLabel;
+@property (strong, nonatomic) QRCodeViewController *QRCVC;
+
+- (IBAction)scanQRCodeBtn:(id)sender;
+- (IBAction)readQRCodeDataBtn:(id)sender;
+
 
 @end
 
@@ -18,6 +30,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // 可由此產生 QRCode: http://www.qr-123.com/zh-TW
+    
+    //
+    [_messageLabel setText:@"按下QRCode進行掃描"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +43,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)scanQRCodeBtn:(id)sender {
+    if(IOS7){
+        if (_QRCVC == nil) {
+            _QRCVC = [[QRCodeViewController alloc] init];
+        }
+        [self presentViewController:_QRCVC animated:YES completion:nil];
+    }
+    else{
+        [_messageLabel setText:@"僅支援iOS7"];
+    }
+}
+
+- (IBAction)readQRCodeDataBtn:(id)sender {
+    [_qrcodedataLabel setText:[_QRCVC returnData]];
+}
 @end
